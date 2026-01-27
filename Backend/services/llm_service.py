4f5219bc34e6f04b17d3b9617067c025bfef9195
syedshoaibahmed48 <syedshoaibahmed48@gmail.com>
2026-01-27 17:12:37 +0300

    Switch LLM from Qwen 2.5 3B instruct to Gemma 3 4B

--- Backend/services/llm_service.py
+++ Backend/services/llm_service.py
@@ -21,10 +21,17 @@ def format_thread_for_prompt(thread: List[Email]) -> str:
 
 def get_llm_response(thread: List[Email], chat_messages: List[dict]) -> str:
 
-    llm_prompt = [
-        {"role": "system", "content": "You are an intelligent email assistant; Your job is to help the user understand, summarize, translate, or respond to emails using ONLY the information provided in the email thread and the chat history. respond to the latest user message in plain text only, reply in a professional, concise, friendly toney, do not make assumptions or add external details, keep the response relevant and actionable, and politely ask for clarification if the user input is unclear."},
-        {"role": "user", "content": f"Here is the email thread:\n{format_thread_for_prompt(thread)}"}
-    ] + chat_messages
+    system_message = (
+        "You are an intelligent email assistant; Your job is to help the user understand, "
+        "summarize, translate, or respond to emails using ONLY the information provided in the "
+        "email thread and the chat history. Respond to the latest user message in plain text only, "
+        "reply in a professional, friendly tone, do not make assumptions or add external "
+        "details, keep the response relevant and actionable, and politely ask for clarification if "
+        "the user input is unclear. Here is the email thread:\n" + format_thread_for_prompt(thread)
+    )
+
+    llm_prompt = [{"role": "system", "content": system_message}] + chat_messages
+
 
     url =  f"{os.getenv("LLM_SERVER_URL", "http://localhost:8080")}/v1/chat/completions"
     payload = {
